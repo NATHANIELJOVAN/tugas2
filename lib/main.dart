b26b96547c3effcede805b5d990e9661c8b8d844
NATHANIELJOVAN <jovan.nathaniel@gmail.com>
2025-09-22 02:11:54 +0700

    grid

--- lib/main.dart
+++ lib/main.dart
@@ -1,85 +1,101 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: MyHomePage(title: "Flutter Demo Home Page"),
-    );
-  }
+  State<MyApp> createState() => _MyAppState();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class _MyAppState extends State<MyApp> {
+  bool isPlaying = false;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text(widget.title)),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image.asset(
-              'assets/image/legends never die.jpg',
-              height: 200,
-              width: 200,
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey[900],
+        appBar: AppBar(
+          title: const Text("Grid View Spotify Style"),
+          backgroundColor: Colors.purple,
+        ),
+        body: Stack(
+          children: [
+            // GridView
+            GridView.extent(
+              maxCrossAxisExtent: 200,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+              primary: false,
+              padding: const EdgeInsets.all(16),
+              children: <Widget>[
+                _albumBox('assets/images/scorpion.jpeg'),
+                _albumBox('assets/images/more life.jpeg'),
+                _albumBox('assets/images/views.jpeg'),
+                _albumBox('assets/images/ss4u.jpeg'),
+                _albumBox('assets/images/clb.jpeg'),
+                _albumBox('assets/images/honestly.jpeg'),
+                _albumBox('assets/images/fatd.jpeg'),
+                _albumBox('assets/images/take care.jpeg'),
+              ],
             ),
-            const SizedBox(height: 16),
-            const Text(
-              'You have pushed the button this many times:',
-              style: TextStyle(
-                fontSize: 16.0,
-                fontWeight: FontWeight.bold,
-                color: Colors.lightBlue,
-                letterSpacing: 1.5,
-                wordSpacing: 1.5,
-              ),
-            ),
-            Text(
-              '$_counter',
-              style: const TextStyle(
-                fontSize: 26.0,
-                fontWeight: FontWeight.bold,
-                color: Colors.red,
+            // Bottom Player
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                margin: const EdgeInsets.all(20),
+                padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    const Icon(Icons.shuffle, color: Colors.white, size: 26),
+                    const Icon(Icons.skip_previous, color: Colors.white, size: 30),
+                    IconButton(
+                      icon: Icon(
+                        isPlaying ? Icons.pause_circle : Icons.play_circle,
+                        color: Colors.white,
+                        size: 38,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          isPlaying = !isPlaying;
+                        });
+                      },
+                    ),
+                    const Icon(Icons.skip_next, color: Colors.white, size: 30),
+                    const Icon(Icons.repeat, color: Colors.white, size: 26),
+                  ],
+                ),
               ),
             ),
           ],
         ),
       ),
-      bottomNavigationBar: BottomAppBar(
-        shape: const CircularNotchedRectangle(),
-        child: Container(height: 50.0),
+    );
+  }
+
+  Widget _albumBox(String imagePath) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.black,
+        border: Border.all(color: Colors.white70, width: 2),
+        borderRadius: BorderRadius.circular(8),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(
-          Icons.audiotrack,
-          color: Colors.green,
-          size: 30.0,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(6),
+        child: Image.asset(
+          imagePath,
+          fit: BoxFit.cover,
         ),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
